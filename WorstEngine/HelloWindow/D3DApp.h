be196c8e945d4fc6dd58ec19420857ec1a58326c
Yorda <921864047@qq.com>
2021-09-16 13:21:39 +0900

    update!
    const buffer, vertex buffering, frame buffering , bundle

---
+++ WorstEngine/HelloWindow/D3DApp.h
@@ -0,0 +1,116 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+#pragma once
+
+#include "DXSample.h"
+
+using namespace DirectX;
+// Note that while ComPtr is used to manage the lifetime of resources on the CPU,
+// it has no understanding of the lifetime of resources on the GPU. Apps must account
+// for the GPU lifetime of resources to avoid destroying objects that may still be
+// referenced by the GPU.
+// An example of this can be found in the class method: OnDestroy().
+using Microsoft::WRL::ComPtr;
+
+class D3DApp : public DXSample
+{
+public:
+    D3DApp(UINT width, UINT height, std::wstring name);
+
+    virtual void OnInit();
+    virtual void OnUpdate();
+    virtual void OnRender();
+    virtual void OnDestroy();
+
+private:
+    static const UINT FrameCount = 4;
+    struct Vertex
+    {
+        XMFLOAT3 position;
+        XMFLOAT4 color;
+
+    };
+    struct SceneConstantBuffer
+
+    {
+
+        XMFLOAT4 offset;
+
+        float padding[60]; // 256×Ö½Ú¶ÔÆë
+
+    };
+
+
+    // Rects
+    CD3DX12_VIEWPORT m_viewportRect;
+    CD3DX12_RECT m_scissorRect;
+
+    //Swap chain
+    ComPtr<IDXGISwapChain3> m_swapChain;
+
+    //devie
+    ComPtr<ID3D12Device> m_device;
+
+    //render targets
+    ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+
+    //Allocators 
+    ComPtr<ID3D12CommandAllocator> m_commandAllocators[FrameCount];
+    ComPtr<ID3D12CommandAllocator>  m_bundleAllocator;
+
+    //Command queue
+    ComPtr<ID3D12CommandQueue> m_commandQueue;
+   
+    
+    //command list
+    ComPtr<ID3D12GraphicsCommandList> m_commandList;
+    ComPtr<ID3D12GraphicsCommandList> m_bundle;
+    //Heaps on GPU
+    ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+    ComPtr<ID3D12DescriptorHeap> m_cbvHeap;
+    
+    //pipeline state object
+    ComPtr<ID3D12PipelineState> m_pipelineState;
+    
+
+    //signature
+    ComPtr<ID3D12RootSignature> m_rootSignature;
+
+
+    //descriptor size
+    UINT m_rtvDescriptorSize;
+
+
+
+
+
+
+
+    // Synchronization objects.
+    UINT m_frameIndex;
+    HANDLE m_fenceEvent;
+    ComPtr<ID3D12Fence> m_fence;
+    UINT64 m_fenceValues[FrameCount];
+
+    //App resources and relatives
+    ComPtr<ID3D12Resource> m_vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+    ComPtr<ID3D12Resource> m_constantBuffer;
+    SceneConstantBuffer m_constantBufferData;
+    UINT8* m_pCbvDataBegin;
+
+    void LoadPipeline();
+    void LoadAssets();
+    void PopulateCommandList();
+    void WaitForGPU();
+    void MoveToNextFrame();
+};
